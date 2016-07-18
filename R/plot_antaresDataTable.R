a3f980cc0abff3891421293f7c0777ef6642193d
françois GUILLEM <francois.guillem@rte-france.com>
2016-07-18 14:41:17 +0200

    BUGFIX: plot.antaresTable was crashing when "variable" was not specified

--- R/plot_antaresDataTable.R
+++ R/plot_antaresDataTable.R
@@ -129,7 +129,7 @@ plot.antaresDataTable <- function(x, variable, main, ylab, ...) {
     
     if (!exists("uniqueColvar")) {
       g <- g %>% hc_xAxis(categories = dt$colvar) %>%
-      hc_add_serie(g, name = variable, data = dt$var, type = "column") 
+      hc_add_serie(name = variable, data = dt$var, type = "column") 
     } else {
       g <- g %>% hc_xAxis(categories = dt[suffix == "", colvar]) %>%
         hc_add_serie(name = variable, data = dt[suffix == "", var], type = "column") %>% 
