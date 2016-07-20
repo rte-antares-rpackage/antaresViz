97b0a22d818793461c8fa1638fdabfcd39c77a18
françois GUILLEM <francois.guillem@rte-france.com>
2016-07-20 11:12:50 +0200

    Add comments for future developers

--- R/productionStack.R
+++ R/productionStack.R
@@ -111,16 +111,34 @@ productionStack <- function(x, variables = "eco2mix", colors = NULL, areas = NUL
 #' 
 #' @return 
 #'   an htmlWidget created with function "dygraph"
+#'   
+#' @note 
+#' When series have positive and negative values, stacked area graphs are not
+#' clearly defined. In our case we want positive values shown in the part of
+#' the graph above 0 and the negative values below 0. To achieve that, we have
+#' to hack the default behavior of dygraphs:
+#' 
+#' 1 - for each time step, sum all negative values and plot the corresponding
+#' area in white
+#' 2 - plot the areas corresponding to the negative values of each series as if
+#' they were positive. This will completely cover the area drawn in step 1.
+#' 3 - plot areas corresponding to the positive values of each series.
+#'
+#' Notice that dygraphs plot series in reverse order, so the data table we need
+#' to create must contain a time column, then the positive values of each
+#' series, then the negative values of each column (absolute values) and finally
+#' a column with the total of negative values.
 #' 
 #' @noRd
 .plotProductionStack <- function(x, variables, colors) {
+
   timeStep <- attr(x, "timeStep")
   
+  # 1- Create a data.table containing the series defined by parameter "variables"
   dt <- data.table(timeId = x$timeId, area = x$area)
   dt$time <- .timeIdToDate(dt$timeId, timeStep)
   dt[, timeId := NULL]
   dt[, c(rev(names(variables)), paste0("neg", names(variables))) := 0]
-  
   
   nvar <- length(variables)
   
@@ -129,11 +147,11 @@ productionStack <- function(x, variables = "eco2mix", colors = NULL, areas = NUL
     set(dt, j = nvar + 3L - i, value = values)
   }
   
-  # Group by timeId
+  # 2- Group by timeId
   dt[, area := NULL]
   dt <- dt[, lapply(.SD, sum), by = time]
   
-  # Separate positive and negative values
+  # 3- Separate positive and negative values and compute total negative values
   totalNeg <- rep(0, nrow(dt))
   
   for (i in length(variables):1) {
@@ -148,7 +166,7 @@ productionStack <- function(x, variables = "eco2mix", colors = NULL, areas = NUL
   
   dt[, totalNeg := totalNeg]
   
-  
+  # 4- Finally plot !!
   colors <- c("#FFFFFF", rev(colors), colors)
   
   dygraph(dt)  %>%
