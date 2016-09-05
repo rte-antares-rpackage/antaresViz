ca00e223669382170411e016ec27f4203c0af439
françois GUILLEM <francois.guillem@rte-france.com>
2016-09-05 16:45:44 +0200

    Add date range selector to exchangesStack

--- R/exchangesStack.R
+++ R/exchangesStack.R
@@ -1,4 +1,4 @@
-exchangesStack <- function(x, area = NULL, colors = NULL, 
+exchangesStack <- function(x, area = NULL, dateRange = NULL, colors = NULL, 
                             main = NULL, ylab = NULL, unit = c("MWh", "GWh", "TWh"),
                             width = "100%", height = "500px",
                             interactive = base::interactive(), 
@@ -6,23 +6,31 @@ exchangesStack <- function(x, area = NULL, colors = NULL,
                             legendItemsPerRow = 5) {
   
   unit <- match.arg(unit)
+  timeStep <- attr(x, "timeStep")
+  opts <- simOptions(x)
   
-  linksDef <- getLinks(namesOnly = FALSE, withDirection = TRUE, opts = simOptions(x))
+  dataDateRange <- as.Date(.timeIdToDate(range(x$timeId), timeStep, opts))
+  if (length(dateRange) < 2) dateRange <- dataDateRange
+  
+  linksDef <- getLinks(namesOnly = FALSE, withDirection = TRUE, opts = opts)
   linksDef <- linksDef[link %in% x$link]
   areaList <- linksDef[, unique(area)]
   
   if (is.null(area)) area = areaList[1]
   
-  plotFun <- function(area, unit) {
+  plotFun <- function(area, dateRange, unit) {
+    # Prepare data for stack creation
     linksDef <- getLinks(area, opts = simOptions(x), namesOnly = FALSE,
                          withDirection = TRUE)
-    dt <- merge(x[, .(link, timeId, flow = `FLOW LIN.`)],
+    dt <- merge(x[as.Date(.timeIdToDate(timeId, timeStep)) %between% dateRange,
+                  .(link, timeId, flow = `FLOW LIN.`)],
                 linksDef,
                 by = "link")
     dt[, flow := flow * direction]
     
     dt <- dcast(dt, timeId ~ to, value.var = "flow")
     
+    # Graphical parameters
     if (is.null(main)) main <- paste("Flows from/to", area)
     if (is.null(ylab)) ylab <- sprintf("Flows (%s)", unit)
     if (is.null(colors)) {
@@ -31,11 +39,15 @@ exchangesStack <- function(x, area = NULL, colors = NULL,
       colors <- rep(colors, length.out = ncol(dt - 1))
     }
     
-    g <- .plotStack(dt, attr(x, "timeStep"), simOptions(x), colors,
+    # Stack
+    g <- .plotStack(dt, timeStep, opts, colors,
                     legendId = legendId, main = main, ylab = ylab)
     
-    if (!legend) return(g)
+    if (!legend) {
+      return(g)
+    }
     
+    # Add a nice legend
     combineWidgets(vflex = c(1, NA),
                    g,
                    tsLegend(names(dt)[-1], colors,  
@@ -45,11 +57,12 @@ exchangesStack <- function(x, area = NULL, colors = NULL,
     
   }
   
-  if (!interactive) return(plotFun(area, unit))
+  if (!interactive) return(plotFun(area, dateRange, unit))
   
   manipulateWidget(
-    plotFun(area, unit),
+    plotFun(area, dateRange, unit),
     area = mwSelect(areaList, area),
+    dateRange = mwDateRange(dateRange, min = dataDateRange[1], max = dataDateRange[2]),
     unit = mwSelect(c("MWh", "GWh", "TWh"), unit)
   )
   
