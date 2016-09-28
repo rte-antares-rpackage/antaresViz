feb5eb2c9a3297d3011d27b0ea3cff8f30fef053
françois GUILLEM <francois.guillem@rte-france.com>
2016-09-28 14:33:17 +0200

    BUGFIX: incorrect paths for leaflet dependencies

--- R/mapDeps.R
+++ R/mapDeps.R
@@ -1,4 +1,43 @@
  requireDep <- function(map, deps) {
+   mapDeps <- list(
+     
+     updateCircleMarkers = htmlDependency(
+       "updateCircleMarkers",
+       "1.0",
+       src = system.file("leafletPlugins", package = "antaresViz"), 
+       script = "updateCircleMarkers.js"
+     ),
+     
+     directedSegment = htmlDependency(
+       "directedSegment", 
+       "1.0",
+       src = system.file("leafletPlugins", package = "antaresViz"), 
+       script = "directedSegment.js"
+     ),
+     
+     polarChart = htmlDependency(
+       "polarChart",
+       "1.0",
+       src = system.file("leafletPlugins", package = "antaresViz"), 
+       script = "polarChart.js"
+     ),
+     
+     d3 = htmlDependency(
+       "d3",
+       "4.2.3",
+       src = system.file("leafletPlugins", package = "antaresViz"), 
+       script = "d3.min.js"
+     ),
+     
+     shadows = htmlDependency(
+       "shadows",
+       "1.0",
+       src = system.file("leafletPlugins", package = "antaresViz"),
+       script = "svgShadows.js"
+     )
+     
+   )
+   
    depsInMap <- sapply(map$dependencies, function(x) x$name)
    
    for (dep in deps) {
@@ -9,42 +48,3 @@
    
    map
  }
-
-mapDeps <- list(
-  
-  updateCircleMarkers = htmlDependency(
-    "updateCircleMarkers",
-    "1.0",
-    src = system.file("leafletPlugins", package = "antaresViz"), 
-    script = "updateCircleMarkers.js"
-  ),
-  
-  directedSegment = htmlDependency(
-    "directedSegment", 
-    "1.0",
-    src = system.file("leafletPlugins", package = "antaresViz"), 
-    script = "directedSegment.js"
-  ),
-  
-  polarChart = htmlDependency(
-    "polarChart",
-    "1.0",
-    src = system.file("leafletPlugins", package = "antaresViz"), 
-    script = "polarChart.js"
-  ),
-  
-  d3 = htmlDependency(
-    "d3",
-    "4.2.3",
-    src = system.file("leafletPlugins", package = "antaresViz"), 
-    script = "d3.min.js"
-  ),
-  
-  shadows = htmlDependency(
-    "shadows",
-    "1.0",
-    src = system.file("leafletPlugins", package = "antaresViz"),
-    script = "svgShadows.js"
-  )
-  
-)
