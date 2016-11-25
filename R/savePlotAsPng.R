25f73cadb78d7d40ec8f59a880057a33b6375480
françois GUILLEM <francois.guillem@rte-france.com>
2016-11-25 10:16:21 +0100

    Save an htmlwidget as a png file (#8)

---
+++ R/savePlotAsPng.R
@@ -0,0 +1,34 @@
+#' Save interactive plot as a png image
+#' 
+#' This function saves an interactive plot generated with one of the functions of
+#' this package as a png image. The result can then be included in documents or
+#' presentations.
+#' 
+#' @param plot
+#'   A plot generated with one of the functions of this package.
+#' @param filename
+#'   The name of the output file
+#' @param width
+#'   Width of the output file
+#' @param height
+#'   height of the output file
+#' @param ...
+#'   Other parameters passed to function \code{\link[webshot]{webshot}}
+#' 
+#' @return 
+#' The function only creates the required file. Nothing is returned
+#' 
+#' @examples
+#' \dontrun{
+#'   mydata <- readAntares()
+#'   myplot <- plot(mydata, variable = "MRG. PRICE", type = "density")
+#'   savePlotAsPng(myplot, file = "myplot.png")
+#' }
+#' 
+#' @export
+#' 
+savePlotAsPng <- function(plot, file = "Rplot.png", width = 600, height = 480, ...) {
+  tmpfile <- tempfile(fileext = ".html")
+  htmlwidgets::saveWidget(plot, file = tmpfile, selfcontained = FALSE)
+  webshot::webshot(tmpfile, file = file, vwidth = width, vheight = height, ...)
+}
