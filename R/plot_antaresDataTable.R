ef8ee00bac20452dbfb56e3e6f4641614195c528
françois GUILLEM <francois.guillem@rte-france.com>
2017-02-15 16:57:05 +0100

    draft of heatmaps for hourly data

--- R/plot_antaresDataTable.R
+++ R/plot_antaresDataTable.R
@@ -700,24 +700,64 @@ plot.antaresDataList <- function(x, y = NULL, table = NULL, variable = NULL, ele
     stop("Heatmap are only for daily and hourly data")
   }
   
+  wdaysLabels <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+  
   if (timeStep == "daily") {
-    dt$weekId <- .getTimeId(dt$timeId * 24, "weekly", opts)
-    wdaysLabels <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-    dt$wday <- wdaysLabels[lubridate::wday(dt$time)]
     
-    dt <- split(dt, f = droplevels(dt$element))
+    dt$weekId <- .getTimeId(dt$timeId * 24, "weekly", opts)
+    dt$wday <- wdaysLabels[lubridate::wday(dt$time)]
     
     xCategories <- c(wdaysLabels,wdaysLabels)[which(wdaysLabels == opts$firstWeekday) + 0:6]
     
-    plots <- lapply(dt, function(x) {
-      plot_ly(x) %>% config(displayModeBar = FALSE) %>% 
-        add_heatmap(x=~wday, y=~weekId, z=~value) %>% 
-        layout(
-          title = ~element[1],
-          xaxis=list(categoryarray=xCategories, categoryorder="array")
-        )
-    })
+  } else if (timeStep == "hourly") {
+    dt$weekId <- .getTimeId(dt$timeId, "weekly", opts)
+    dt$wday <- wdaysLabels[lubridate::wday(dt$time)]
+    dt$hour <- sprintf("%02.0f:00", lubridate::hour(dt$time))
+    dt$wday <- paste(dt$wday, dt$hour)
     
-    combineWidgets(list=plots)
+    hours <- sprintf("%02.0f:00", 0:23)
+    
+    xCategories <- c(wdaysLabels,wdaysLabels)[which(wdaysLabels == opts$firstWeekday) + 0:6]
+    xCategories <- lapply(xCategories, function(x) {
+      paste(x, hours)
+    })
+    xCategories <- do.call(c, xCategories)
+    
   }
+  
+  dt <- split(dt, f = droplevels(dt$element))
+  
+  plots <- lapply(dt, function(x) {
+    rangevar <- range(x$value)
+    if (rangevar[1] >= 0) {
+      domain <- rangevar
+    } else {
+      domain <- c(-max(abs(rangevar)), max(abs(rangevar)))
+    }
+    
+    colorPalette <- continuousColorPal(x$value, 20, domain)
+    minVal <- rangevar[1]
+    scale <- diff(rangevar)
+    colorScaleFun <- function(x) {
+      print(x)
+      x <- scale * x + minVal
+      col <- attr(colorPalette, "pal")
+      breaks <- attr(colorPalette, "breaks")
+      
+      res <- rep(col[1], length(x))
+      for (i in 2:(length(breaks) - 1)) {
+        res[x > breaks[i]] <- col[i]
+      }
+      res
+    }
+    plot_ly(x) %>% config(displayModeBar = FALSE) %>% 
+      add_heatmap(x=~wday, y=~weekId, z=~value, colors = colorScaleFun) %>% 
+      layout(
+        title = ~element[1],
+        xaxis=list(categoryarray=xCategories, categoryorder="array")
+      )
+  })
+  
+  
+  combineWidgets(list=plots)
 }
