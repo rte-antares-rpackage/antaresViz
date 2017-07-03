a609f85eb5b1e5712d266e7fc2e6f048f7b67496
françois GUILLEM <francois.guillem@rte-france.com>
2017-07-03 17:03:06 +0200

    Add popup to background map (#45)

--- R/map_helpers.R
+++ R/map_helpers.R
@@ -190,9 +190,10 @@
   
   # Update corresponding polygons if necessary
   if (!is.null(ml$map)) {
-    onChange <- JS('(function(opts, d3) {
-        if (opts.fillColor && this._map) {
-          var s = this._map.layerManager.getLayer("shape", this.layerId);
+    onChange <- JS('(function(opts, popup, d3) {
+        var s = this._map.layerManager.getLayer("shape", this.layerId);
+        s.bindPopup(popup);
+        if (opts.fillColor) {
           d3.select(s._path)
             .transition()
             .duration(750)
