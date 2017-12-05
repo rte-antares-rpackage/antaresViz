ff736485d204809d961932a45cff6f086a2d7058
Benoit Thieurmel <bthieurmel@gmail.com>
2017-12-05 11:10:50 +0100

    fix sharing inputs between modules

--- inst/application/ui.R
+++ inst/application/ui.R
@@ -238,16 +238,17 @@ navbarMenu("plotMap",
                     ))
            )
 ), 
-tabPanel("Memory Controls", 
+tabPanel("Parameters", 
          fluidRow(
-           column(3, h4("readAntares RAM limit (in Go) : ")),
+           column(2, checkboxInput("is_shared_input", label = "Share inputs between modules ?", value = TRUE)),
+           column(2, h4("readAntares RAM limit (in Go) : ")),
            column(3, div(numericInput("ram_limit", label = NULL, 
                                       min = 1, max = 10, value = {
                                         if(!is.null(getOption("maxSizeLoad"))){
                                           getOption("maxSizeLoad")
                                         } else {10}
                                       }), align = "center")),
-           column(3, h4("antaresViz data module (in Mb) : ")),
+           column(2, h4("antaresViz data module (in Mb) : ")),
            column(3, div(numericInput("data_module", label = NULL, 
                                       min = 1, max = 10, value = {
                                         if(!is.null(getOption("antaresVizSizeGraph"))){
